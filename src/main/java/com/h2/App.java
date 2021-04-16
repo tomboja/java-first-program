0e36d692102494538a4ac83cab344ad87c20b7bc
Temesgen Dessalegn <Temesgen.Dessalegn@signanthealth.com>
2021-04-17 00:12:47 +0300

    first commit

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -4,14 +4,20 @@ package com.h2;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return 2 * number;
+    }
+
+    private static int add(int[] numbers) {
+        int sum = 0;
+        for(int num : numbers)
+            sum += num;
+        return sum;
     }
 }
---
+++ src/main/java/com/h2/BestLoanRates.java
@@ -0,0 +1,38 @@
+/*
+ * @created on: 16/04/2021 - 23.43
+ * @project: calc
+ * @author: tdessalegn
+ * @author name: Temesgen Dessalegn
+ * ================================
+ *
+ */
+package com.h2;
+
+import java.util.Map;
+import java.util.Scanner;
+
+public class BestLoanRates {
+  public static final Map<Integer, Float> bestRates = Map.of(1, 5.50f, 2, 3.45f, 3, 2.67f);
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter your name: ");
+    String name = scanner.nextLine();
+
+    System.out.print("Enter the loan term (in years): ");
+    int loanTermInYears = scanner.nextInt();
+
+    float bestRate = getRates(loanTermInYears);
+
+    if(bestRate == 0.0f)
+      System.out.println("No available rates for term: " + loanTermInYears + " years");
+    else
+      System.out.println("Best Available Rate: " + getRates(loanTermInYears) + "%");
+
+    scanner.close();
+  }
+
+  public static float getRates(int loanTermInYears) {
+    return bestRates.getOrDefault(loanTermInYears, 0.0f);
+  }
+}
