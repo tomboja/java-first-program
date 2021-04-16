fb0f71abfbfb30cf86e5541427a6a1fe3bc1cfee
Temesgen Dessalegn <Temesgen.Dessalegn@signanthealth.com>
2021-04-17 00:28:04 +0300

    Merge branch 'master' of https://github.com/tomboja/java-first-program
    # Conflicts:
    #	src/main/java/com/h2/BestLoanRates.java

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -18,7 +18,7 @@ public class BestLoanRates {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Enter your name");
     String name = scanner.nextLine();
-    System.out.println(name);
+    System.out.println("Hello " + name);
     System.out.println("Enter the loan term (in years)");
     int loanTermInYears = scanner.nextInt();
 
